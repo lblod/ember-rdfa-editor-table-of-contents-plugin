0e5e7aefe4a9c196db01d21ecb36a5d31a76fc9b
Elena Poelman <elena.poelman@redpencil.io>
2022-06-08 15:19:50 +0200

    added listener to content changes and update counter on each content change

--- addon/components/inline-components/outline.ts
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-import { OutlineEntry } from './table-of-contents';
-
-interface InlineComponentsOutlineArgs {
-  entries: OutlineEntry[];
-}
-
-export default class InlineComponentsOutline extends Component<InlineComponentsOutlineArgs> {}
--- addon/components/inline-components/table-of-contents.ts
+++ addon/components/inline-components/table-of-contents.ts
@@ -1,5 +1,9 @@
-import InlineComponent from '@lblod/ember-rdfa-editor/components/inline-components/inline-component';
-
+import InlineComponent, {
+  InlineComponentArgs,
+} from '@lblod/ember-rdfa-editor/components/inline-components/inline-component';
+import ModelNode from '@lblod/ember-rdfa-editor/model/model-node';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 interface DocumentOutline {
   entries: OutlineEntry[];
 }
@@ -9,11 +13,37 @@ export interface OutlineEntry {
   children?: OutlineEntry[];
 }
 export default class TableOfContentsComponent extends InlineComponent {
-  get documentOutlineDummy(): DocumentOutline {
+  @tracked
+  documentOutline: DocumentOutline;
+
+  get numberOfUpdates(): number {
+    return (this.getStateProperty('numberOfUpdates') as number) || 0;
+  }
+
+  constructor(owner: unknown, args: InlineComponentArgs) {
+    super(owner, args);
+    this.documentOutline = this.getDocumentOutlineDummy();
+
+    this.args.controller.onEvent('contentChanged', this.update.bind(this));
+  }
+
+  willDestroy(): void {
+    console.log('destroy');
+    this.args.controller.offEvent('contentChanged', this.update.bind(this));
+  }
+
+  update() {
+    console.log((this.getStateProperty('numberOfUpdates') as number) || 0);
+    const newVal = this.numberOfUpdates + 1;
+    this.setStateProperty('numberOfUpdates', newVal);
+    this.documentOutline = this.getDocumentOutlineDummy();
+  }
+
+  getDocumentOutlineDummy(): DocumentOutline {
     return {
       entries: [
         {
-          content: 'Title 1',
+          content: `Title 1`,
           children: [
             {
               content: 'Subtitle 1',
@@ -36,5 +66,21 @@ export default class TableOfContentsComponent extends InlineComponent {
         },
       ],
     };
+  }
+
+  extractTitle(node: ModelNode): object {
+    if (ModelNode.isModelElement(node)) {
+      const rdfaContent = {
+        content: node.getRdfaAttributes(),
+        children: node.children.map((child) => this.extractTitle(child)),
+      };
+      // const prefixes = node.getRdfaAttributes();
+      // console.log(prefixes);
+      // node.children.forEach((child) => {
+      //   this.extractTitle(child);
+      // });
+      return rdfaContent;
+    }
+    return {};
   }
 }
