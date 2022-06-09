da46d09cd1f0243ffac361d9f9d825d8f3e020a8
Elena Poelman <elena.poelman@redpencil.io>
2022-06-09 14:17:38 +0200

    dynamic auto-refreshing content works and entries are clickable and jump to the required element

--- addon/components/inline-components/table-of-contents.ts
+++ addon/components/inline-components/table-of-contents.ts
@@ -3,6 +3,9 @@ import InlineComponent, {
 } from '@lblod/ember-rdfa-editor/components/inline-components/inline-component';
 import ModelNode from '@lblod/ember-rdfa-editor/model/model-node';
 import { tracked } from '@glimmer/tracking';
+import ModelNodeUtils from '@lblod/ember-rdfa-editor/model/util/model-node-utils';
+import ModelRange from '@lblod/ember-rdfa-editor/model/model-range';
+import ModelPosition from '@lblod/ember-rdfa-editor/model/model-position';
 import { action } from '@ember/object';
 interface DocumentOutline {
   entries: OutlineEntry[];
@@ -10,19 +13,21 @@ interface DocumentOutline {
 
 export interface OutlineEntry {
   content: string;
+  node: ModelNode;
   children?: OutlineEntry[];
 }
 export default class TableOfContentsComponent extends InlineComponent {
   @tracked
-  documentOutline: DocumentOutline;
-
-  get numberOfUpdates(): number {
-    return (this.getStateProperty('numberOfUpdates') as number) || 0;
-  }
+  documentOutline: DocumentOutline | undefined;
 
   constructor(owner: unknown, args: InlineComponentArgs) {
     super(owner, args);
-    this.documentOutline = this.getDocumentOutlineDummy();
+    const node = this.extractTitle(this.args.controller.modelRoot);
+    if (node) {
+      this.documentOutline = {
+        entries: [{ node, content: ModelNodeUtils.getTextContent(node) }],
+      };
+    }
 
     this.args.controller.onEvent('contentChanged', this.update.bind(this));
   }
@@ -33,54 +38,75 @@ export default class TableOfContentsComponent extends InlineComponent {
   }
 
   update() {
-    console.log((this.getStateProperty('numberOfUpdates') as number) || 0);
-    const newVal = this.numberOfUpdates + 1;
-    this.setStateProperty('numberOfUpdates', newVal);
-    this.documentOutline = this.getDocumentOutlineDummy();
-  }
-
-  getDocumentOutlineDummy(): DocumentOutline {
-    return {
-      entries: [
-        {
-          content: `Title 1`,
-          children: [
-            {
-              content: 'Subtitle 1',
-            },
-            {
-              content: 'Subtitle 2',
-            },
-          ],
-        },
-        {
-          content: 'Title 2',
-          children: [
-            {
-              content: 'Subtitle 3',
-            },
-            {
-              content: 'Subtitle 4',
-            },
-          ],
-        },
-      ],
-    };
-  }
-
-  extractTitle(node: ModelNode): object {
-    if (ModelNode.isModelElement(node)) {
-      const rdfaContent = {
-        content: node.getRdfaAttributes(),
-        children: node.children.map((child) => this.extractTitle(child)),
+    const node = this.extractTitle(this.args.controller.modelRoot);
+    if (node) {
+      this.documentOutline = {
+        entries: [{ node, content: ModelNodeUtils.getTextContent(node) }],
       };
-      // const prefixes = node.getRdfaAttributes();
-      // console.log(prefixes);
-      // node.children.forEach((child) => {
-      //   this.extractTitle(child);
-      // });
-      return rdfaContent;
     }
-    return {};
+  }
+
+  extractRdfaOutline(
+    node: ModelNode
+  ): { content?: object; children?: object[] }[] {
+    if (ModelNode.isModelElement(node)) {
+      const attributes: object = node.getRdfaAttributes() as object;
+      let hasRdfaAttributes = false;
+      for (const k in attributes) {
+        if (k !== 'currentPrefixes') {
+          const val = attributes[k];
+          if (val) {
+            hasRdfaAttributes = true;
+            break;
+          }
+        }
+      }
+      if (hasRdfaAttributes) {
+        if (attributes['properties'].length) {
+          console.log(node);
+          console.log(ModelNodeUtils.getTextContent(node));
+        }
+        const rdfaContent = {
+          content: attributes,
+          children: node.children.flatMap((child) => this.extractTitle(child)),
+        };
+        return [rdfaContent];
+      } else {
+        return node.children.flatMap((child) => this.extractTitle(child));
+      }
+    }
+    return [];
+  }
+
+  extractTitle(node: ModelNode): ModelNode | null {
+    if (ModelNode.isModelElement(node)) {
+      const attributes: object = node.getRdfaAttributes() as object;
+      if (
+        attributes['properties'] &&
+        attributes['properties'].includes(
+          'http://data.europa.eu/eli/ontology#title'
+        )
+      ) {
+        return node;
+      }
+      let result = null;
+      node.children.forEach((child) => {
+        const val = this.extractTitle(child);
+        if (val) {
+          result = val;
+          return;
+        }
+      });
+      return result;
+    }
+    return null;
+  }
+
+  @action
+  moveToSection(node: ModelNode) {
+    console.log('moveToSection');
+    const range = new ModelRange(ModelPosition.fromInNode(node, 0));
+    this.args.controller.selection.selectRange(range);
+    this.args.controller.write();
   }
 }
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,5 @@
 // Types for compiled templates
-declare module '@lblod/ember-rdfa-ic-sample-plugin/templates/*' {
+declare module '@lblod/ember-rdfa-table-of-contents-plugin/templates/*' {
   import { TemplateFactory } from 'htmlbars-inline-precompile';
   const tmpl: TemplateFactory;
   export default tmpl;
