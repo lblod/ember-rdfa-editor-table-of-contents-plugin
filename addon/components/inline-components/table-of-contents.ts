af1cbfc40f7b0013d24a94936230f612dcb83c93
Elena Poelman <elena.poelman@redpencil.io>
2022-06-15 11:04:41 +0200

    update with new inline components API

--- addon/components/inline-components/table-of-contents.ts
+++ addon/components/inline-components/table-of-contents.ts
@@ -1,6 +1,3 @@
-import InlineComponent, {
-  InlineComponentArgs,
-} from '@lblod/ember-rdfa-editor/components/inline-components/inline-component';
 import ModelNode from '@lblod/ember-rdfa-editor/model/model-node';
 import { tracked } from '@glimmer/tracking';
 import ModelNodeUtils from '@lblod/ember-rdfa-editor/model/util/model-node-utils';
@@ -8,6 +5,8 @@ import { action } from '@ember/object';
 // import '@lblod/ember-rdfa-editor/types/lblod/marawa/rdfa-attributes';
 import RdfaAttributes from '@lblod/marawa/rdfa-attributes';
 import { TOC_CONFIG } from '@lblod/ember-rdfa-table-of-contents-plugin/utils/toc_config';
+import Component from '@glimmer/component';
+import { InlineComponentArgs } from '@lblod/ember-rdfa-editor/model/inline-components/inline-component-controller';
 interface DocumentOutline {
   entries: OutlineEntry[];
 }
@@ -17,7 +16,7 @@ export interface OutlineEntry {
   node: ModelNode;
   children?: OutlineEntry[];
 }
-export default class TableOfContentsComponent extends InlineComponent {
+export default class TableOfContentsComponent extends Component<InlineComponentArgs> {
   @tracked
   documentOutline: DocumentOutline | undefined;
 
@@ -25,26 +24,35 @@ export default class TableOfContentsComponent extends InlineComponent {
     super(owner, args);
     this.update();
 
-    this.args.controller.onEvent('contentChanged', this.update.bind(this));
-    this.args.controller.onEvent('modelRead', this.update.bind(this));
+    this.args.editorController.onEvent(
+      'contentChanged',
+      this.update.bind(this)
+    );
+    this.args.editorController.onEvent('modelRead', this.update.bind(this));
   }
 
   willDestroy(): void {
-    console.log('destroy');
-    this.args.controller.offEvent('contentChanged', this.update.bind(this));
-    this.args.controller.offEvent('modelRead', this.update.bind(this));
+    this.args.editorController.offEvent(
+      'contentChanged',
+      this.update.bind(this)
+    );
+    this.args.editorController.offEvent('modelRead', this.update.bind(this));
+    super.willDestroy();
   }
 
   update() {
     const outline: DocumentOutline = {
-      entries: this.extractOutline(this.args.controller.modelRoot),
+      entries: this.extractOutline(this.args.editorController.modelRoot),
     };
     this.documentOutline = outline;
   }
 
   @action
   remove() {
-    this.args.controller.executeCommand('remove-component', this.args.model);
+    this.args.editorController.executeCommand(
+      'remove-component',
+      this.args.componentController.model
+    );
   }
 
   extractOutline(node: ModelNode): OutlineEntry[] {
@@ -61,7 +69,7 @@ export default class TableOfContentsComponent extends InlineComponent {
               break;
             } else {
               const nodes = [
-                ...this.args.controller.datastore
+                ...this.args.editorController.datastore
                   .match(
                     `>${attributes.resource}`,
                     tocConfigEntry.value.predicate
@@ -97,8 +105,8 @@ export default class TableOfContentsComponent extends InlineComponent {
   @action
   moveToSection(node: ModelNode) {
     console.log('moveToSection');
-    const range = this.args.controller.rangeFactory.fromInNode(node, 0);
-    this.args.controller.selection.selectRange(range);
-    this.args.controller.write(true, true);
+    const range = this.args.editorController.rangeFactory.fromInNode(node, 0);
+    this.args.editorController.selection.selectRange(range);
+    this.args.editorController.write(true, true);
   }
 }
--- addon/models/inline-components/table-of-contents.ts
+++ addon/models/inline-components/table-of-contents.ts
@@ -3,8 +3,24 @@ import {
   InlineComponentSpec,
   Properties,
 } from '@lblod/ember-rdfa-editor/model/inline-components/model-inline-component';
-
+import { DomNodeMatcher } from '@lblod/ember-rdfa-editor/model/mark';
+import { AttributeSpec } from '@lblod/ember-rdfa-editor/model/util/render-spec';
+import { isElement } from '@lblod/ember-rdfa-editor/utils/dom-helpers';
 export default class TableOfContentsSpec extends InlineComponentSpec {
+  matcher: DomNodeMatcher<AttributeSpec> = {
+    tag: this.tag,
+    attributeBuilder: (node) => {
+      if (isElement(node)) {
+        if (
+          node.classList.contains('inline-component') &&
+          node.classList.contains(this.name)
+        ) {
+          return {};
+        }
+      }
+      return null;
+    },
+  };
   _renderStatic(_props?: Properties): string {
     return `
       <p>Table of Contents</p>
